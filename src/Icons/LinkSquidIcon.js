335cbb3501c2af987b16e90ea1816ccaaacd7c0e
Oli Nelson <olivernelson@icloud.com>
2020-03-23 13:50:04 +1100

    file name

--- src/Icons/LinkSquidIcon.js
+++ src/Icons/LinkSquidIcon.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import linkSquidLogo from '../data/images/linkSquidLogo.png'
+import linkSquidLogo from '../data/images/linkSquidLogo2.png'
 export default () => (
   <img style={{ width: '2.5rem', height: 'auto' }} src={linkSquidLogo} />
 )
