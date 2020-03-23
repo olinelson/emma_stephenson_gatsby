e1b8c91d9944558cbc87011892624f9e72832093
Oli Nelson <olivernelson@icloud.com>
2020-03-23 13:52:10 +1100

    fixed squid height

--- src/Icons/LinkSquidIcon.js
+++ src/Icons/LinkSquidIcon.js
@@ -1,5 +1,5 @@
 import React from 'react'
 import linkSquidLogo from '../data/images/linkSquidLogo2.png'
 export default () => (
-  <img style={{ width: '2.5rem', height: 'auto' }} src={linkSquidLogo} />
+  <img style={{ width: '2.5rem', height: '2.5rem' }} src={linkSquidLogo} />
 )
