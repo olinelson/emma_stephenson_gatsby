c37537575cbf63b52a4b6958d779047e1a5a6923
Oli Nelson <oliver.nelson@nirovision.com>
2022-06-14 23:32:56 +1000

    Add ga

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,49 +1,77 @@
 module.exports = {
   siteMetadata: {
-    title: 'Emma Stephenson',
-    description: 'Australian songwriter, pianist and piano teacher',
-    author: 'olinelson'
+    title: "Emma Stephenson",
+    description: "Australian songwriter, pianist and piano teacher",
+    author: "olinelson",
   },
   plugins: [
-    'gatsby-plugin-react-helmet',
+    "gatsby-plugin-react-helmet",
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
-        path: `${__dirname}/src/data`
-      }
+        path: `${__dirname}/src/data`,
+      },
     },
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',
-    'gatsby-transformer-remark',
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-remark",
     {
-      resolve: 'gatsby-plugin-less',
+      resolve: "gatsby-plugin-less",
       options: {
         javascriptEnabled: true,
         modifyVars: {
-          'font-size-base': '16px',
-          'primary-color': '#ea2940'
-        }
-      }
+          "font-size-base": "16px",
+          "primary-color": "#ea2940",
+        },
+      },
     },
 
     {
-      resolve: 'gatsby-plugin-manifest',
+      resolve: "gatsby-plugin-manifest",
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
-        start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
-        display: 'minimal-ui',
-        icon: 'src/data/images/gatsby-icon.png' // This path is relative to the root of the site.
-      }
+        name: "gatsby-starter-default",
+        short_name: "starter",
+        start_url: "/",
+        background_color: "#663399",
+        theme_color: "#663399",
+        display: "minimal-ui",
+        icon: "src/data/images/gatsby-icon.png", // This path is relative to the root of the site.
+      },
     },
     {
-      resolve: 'gatsby-plugin-styled-components',
+      resolve: "gatsby-plugin-styled-components",
       options: {
         // Add any options here
-      }
-    }
-
-  ]
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-BQCPG1WWYZ", // Google Analytics / GA
+          // "AW-CONVERSION_ID", // Google Ads / Adwords / AW
+          // "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          // optimize_id: "OPT_CONTAINER_ID",
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          // exclude: ["/preview/**", "/do-not-track/me/too/"],
+          // Defaults to https://www.googletagmanager.com
+          // origin: "YOUR_SELF_HOSTED_ORIGIN",
+        },
+      },
+    },
+  ],
 }
